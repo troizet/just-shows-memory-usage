27ab90de725dab0ececc9e70c78fe0138fbf5701
Alexey Borokhvostov <troizet@gmail.com>
2024-09-07 23:34:22 +0700

    Added the ability to customize the number of decimal places and refresh time.

--- extension.js
+++ extension.js
@@ -6,63 +6,79 @@ import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
-var indicator, label, timeout;
-
 export default class JustShowsMemoryExtension extends Extension {
-   _refresh() {
-      let info = this.read_proc_meminfo();
-      let total = (info.total/(1024*1024)).toFixed(0) + "G";
-      let used = (info.used/(1024*1024)).toFixed(0) + "G";
+    _refresh() {
+        let info = this.read_proc_meminfo();
+        let total = (info.total / (1024 * 1024)).toFixed(this._digits) + "G";
+        let used = (info.used / (1024 * 1024)).toFixed(this._digits) + "G";
 
-      label.set_text(used + '/' + total);
-   }
-
-   enable() {
-      indicator = new PanelMenu.Button(0.0, "Just memory usage", false);
-
-      timeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
-          this._refresh();
-          return true;
-      });
-      Main.panel.addToStatusArea("Just memory usage Indicator", indicator);
-
-      label = new St.Label({
-        text: 'Just memory usage',
-        y_align: Clutter.ActorAlign.CENTER,
-        style_class: 'panel-button',
-        track_hover: false,
-        reactive: false
-      });
-
-      indicator.actor.add_child(label);
-   }
-
-   disable() {
-      GLib.source_remove(timeout);
-      indicator.destroy();
-      indicator = null;
-      label = null;
-      timeout = null;
-   }
-
-   read_proc_meminfo() {
-      let result = GLib.file_get_contents("/proc/meminfo");
-      if (result[0]) {
-        let str = new TextDecoder().decode(result[1]);
-        let lines = str.split("\n");
-        let total = this.getMemInfo(lines[0]);
-        let avail = this.getMemInfo(lines[2]);
-
-        GLib.free(result[1]);
-        return {total: total, used: (total-avail)};
-      } else {
-        log(`${this.name}: Error to read /proc/meminfo`);
-      }
+        this._label.set_text(used + '/' + total);
     }
 
-   getMemInfo(str) {
-      let reg = /\d+/i;
-      let s = str.match(reg);
-      return parseInt(s[0]);
+    enable() {
+        this._indicator = new PanelMenu.Button(0.0, "Just memory usage", false);
+        this._settings = this.getSettings();
+        this._digits = this._settings.get_uint('digits');
+
+        this._timeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, this._settings.get_uint('timeout'), () => {
+            this._refresh();
+            return true;
+        });
+        Main.panel.addToStatusArea("Just memory usage Indicator", this._indicator);
+
+        this._indicator.menu.addAction(_('Preferences'),
+                () => this.openPreferences());
+
+        this._settings.connect('changed::digits', (settings, key) => {
+            this._digits = settings.get_uint(key);
+        });
+
+        this._settings.connect('changed::timeout', (settings, key) => {
+            GLib.source_remove(this._timeout);
+
+            this._timeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, settings.get_uint(key), () => {
+                this._refresh();
+                return true;
+            });
+        });
+
+        this._label = new St.Label({
+            text: 'Just memory usage',
+            y_align: Clutter.ActorAlign.CENTER,
+            style_class: 'panel-button',
+            track_hover: false,
+            reactive: false
+        });
+
+        this._indicator.actor.add_child(this._label);
+    }
+
+    disable() {
+        GLib.source_remove(this._timeout);
+        this._indicator.destroy();
+        this._indicator = null;
+        this._label = null;
+        this._timeout = null;
+    }
+
+    read_proc_meminfo() {
+        let result = GLib.file_get_contents("/proc/meminfo");
+        if (result[0]) {
+            let str = new TextDecoder().decode(result[1]);
+            let lines = str.split("\n");
+            let total = this.getMemInfo(lines[0]);
+            let avail = this.getMemInfo(lines[2]);
+
+            GLib.free(result[1]);
+            return {total: total, used: (total - avail)};
+        } else {
+            log(`${this.name}: Error to read /proc/meminfo`);
+        }
+    }
+
+    getMemInfo(str) {
+        let reg = /\d+/i;
+        let s = str.match(reg);
+        return parseInt(s[0]);
     }
 }
---
+++ prefs.js
@@ -0,0 +1,54 @@
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
+import Adw from 'gi://Adw';
+
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+
+export default class JustShowsMemoryPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        const page = new Adw.PreferencesPage();
+        window.add(page);
+
+        const group = new Adw.PreferencesGroup();
+        page.add(group);
+
+        const digitsSpin = new Adw.SpinRow({
+            title: _('Digits'),
+            subtitle: _('Number of displayed decimal digits.')
+        });
+
+        digitsSpin.set_adjustment(
+            new Gtk.Adjustment({
+                lower: 0,
+                upper: 5,
+                step_increment: 1
+            })
+        );
+
+        group.add(digitsSpin);
+
+        const timeoutSpin = new Adw.SpinRow({
+            title: _('Timeout'),
+            subtitle: _('Timeout setting.')
+        });
+
+        timeoutSpin.set_adjustment(
+            new Gtk.Adjustment({
+                lower: 100,
+                upper: 10000,
+                step_increment: 100
+            })
+        );
+
+        group.add(timeoutSpin);
+
+        window._settings = this.getSettings();
+
+        window._settings.bind('digits', digitsSpin, 'value',
+            Gio.SettingsBindFlags.DEFAULT);
+
+        window._settings.bind('timeout', timeoutSpin, 'value',
+            Gio.SettingsBindFlags.DEFAULT);
+    }
+}
